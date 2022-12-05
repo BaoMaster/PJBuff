1a559234bad99e9b9e1b292867b5549298a8f021
BaoNguyen <bao.nguyen@nois.vn>
2022-12-05 17:24:11 +0700

    WIP add axios

---
+++ src/config/FetchInterceptor.ts
@@ -0,0 +1,101 @@
+import axios from "axios";
+import history from "./history";
+import { notification } from "antd";
+
+const service = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 60000,
+});
+
+// Config
+const ENTRY_ROUTE = "/auth/login";
+const TOKEN_PAYLOAD_KEY = "Authorization";
+const PUBLIC_REQUEST_KEY = "public-request";
+const AUTH_TOKEN = "token";
+
+let notificationParam: any = {
+  message: "Authentication Fail",
+};
+
+// API Request interceptor
+service.interceptors.request.use(
+  (config) => {
+    config.headers = config.headers || {};
+    config.headers["Content-Type"] = config.headers["Content-Type"] || "application/json";
+    
+    const jwtToken = localStorage.getItem(AUTH_TOKEN);
+    if (jwtToken) {
+      config.headers[TOKEN_PAYLOAD_KEY] = "Bearer " + jwtToken;
+    }
+
+    if (!jwtToken && !config.headers[PUBLIC_REQUEST_KEY]) {
+      history.push(ENTRY_ROUTE);
+      // window.location.reload();
+    }
+
+    return config;
+  },
+  (error) => {
+    // Do something with request error here
+    notification.error({
+      message: "Error",
+    });
+    Promise.reject(error);
+  }
+);
+
+// API respone interceptor
+service.interceptors.response.use(
+  (response) => {
+    if (response.data?.message) notificationParam.message = response.data?.message;
+
+    // notification.success(notificationParam);
+    return response.data;
+  },
+  (error) => {
+    // Remove token and redirect
+    if (error.response) {
+      if (error.response.status === 400 || error.response.status === 403) {
+        if (error.response.data?.message) notificationParam.message = error.response.data?.message;
+
+        const originalConfig = error.config;
+        if (originalConfig.url === "/accounts/sign-in") {
+          notificationParam.description = "Please login again";
+          localStorage.removeItem(AUTH_TOKEN);
+          history.push(ENTRY_ROUTE);
+        } else {
+          //window.location.reload();
+        }
+      }
+
+      if (error.response.status === 401) {
+        history.push(ENTRY_ROUTE);
+        window.location.reload();
+      }
+
+      if (error.response.status === 404) {
+        notificationParam.message = "Not Found";
+      }
+
+      if (error.response.status === 405) {
+        notificationParam.message = "Method Not Allowed";
+      }
+
+      if (error.response.status === 500) {
+        notificationParam.message = "Internal Server Error";
+      }
+
+      if (error.response.status === 508) {
+        notificationParam.message = "Time Out";
+      }
+    } else {
+      notificationParam.message = "Error";
+    }
+
+    notification.error(notificationParam);
+
+    return Promise.reject(error);
+  }
+);
+
+export default service;
