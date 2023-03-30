baa6f41394dfb0b2ac7e1828226ab3be74259eb8
biaoHuynh <hnquocbao22@gmail.com>
2023-03-30 14:46:18 +0700

    add click

--- src/pages/GoogleMapsPage/GoogleMapsPage.tsx
+++ src/pages/GoogleMapsPage/GoogleMapsPage.tsx
@@ -76,47 +76,88 @@ const GoogleMaps: React.FC = () => {
       map,
       title: 'IUH',
     });
-
+    marker.addListener('click', () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
     const marker2 = new maps.Marker({
       position: { lat: 10.819549976998982, lng: 106.68300492634789 },
       map,
       title: 'Cửa hàng thức ăn thú cưng Pet Likes',
     });
-
+    marker2.addListener('click', () => {
+      infowindow2.open({
+        anchor: marker2,
+        map,
+      });
+    });
     const marker3 = new maps.Marker({
       position: { lat: 10.82281746092701, lng: 106.68575211854676 },
       map,
       title: 'Bác Sĩ Thú Y',
     });
-
+    marker3.addListener('click', () => {
+      infowindow3.open({
+        anchor: marker3,
+        map,
+      });
+    });
     const marker4 = new maps.Marker({
       position: { lat: 10.819968208084344, lng: 106.6896903790092 },
       map,
       title: 'Tiệm Nhà Sâu',
     });
-
+    marker4.addListener('click', () => {
+      infowindow4.open({
+        anchor: marker4,
+        map,
+      });
+    });
     const marker5 = new maps.Marker({
       position: { lat: 10.820975045636674, lng: 106.69377684783682 },
       map,
       title: 'Tiệm Thú Cưng - Pet VN',
     });
-
+    marker5.addListener('click', () => {
+      infowindow5.open({
+        anchor: marker5,
+        map,
+      });
+    });
     const marker6 = new maps.Marker({
       position: { lat: 10.82354833575151, lng: 106.69024769363888 },
       map,
       title: 'Kiki Puppey Dịch Vụ, Thức ăn, Chăm Sóc Thú Cưng',
     });
-
+    marker6.addListener('click', () => {
+      infowindow6.open({
+        anchor: marker6,
+        map,
+      });
+    });
     const marker7 = new maps.Marker({
       position: { lat: 10.816749594737194, lng: 106.68822303405939 },
       map,
       title: 'Choowie Pet Shop',
     });
-
+    marker7.addListener('click', () => {
+      infowindow7.open({
+        anchor: marker7,
+        map,
+      });
+    });
     const marker8 = new maps.Marker({
       position: { lat: 10.812796422391976, lng: 106.68686047190828 },
       map,
       title: 'Pet House - Spa Chăm Sóc Làm Đẹp Chó Mèo Giá Rẻ Tại Bình Thạnh',
+    });
+    marker8.addListener('click', () => {
+      infowindow8.open({
+        anchor: marker8,
+        map,
+      });
     });
   };
 
