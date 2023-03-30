fbeeb311dc0a90753a101a991098d300a57dffb8
biaoHuynh <hnquocbao22@gmail.com>
2023-03-30 16:49:13 +0700

    change witdh heigt fit page

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -4,8 +4,8 @@ import * as s from './Tables.styles';
 import img from './AboutUss.png';
 const Dashboard: React.FC = () => {
   return (
-    <s.Card2 >
-      <img src={img} alt="about us" />
+    <s.Card2>
+      <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={img} alt="about us" />
     </s.Card2>
   );
 };
