157d0cf97092a72fb105fabd54757b3c42757dc4
Huỳnh Nguyễn Quốc Bảo <hnquocbao22@gmail.com>
2023-04-03 01:07:06 +0700

    img

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import * as s from './Tables.styles';
 import img from './AboutUss.png';
+import img2 from './vivu2.png';
 const Dashboard: React.FC = () => {
   return (
-    <s.Card2>
-      <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={img} alt="about us" />
-    </s.Card2>
+    <>
+      <s.Card2>
+        <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={img} alt="about us" />
+      </s.Card2>
+      <s.Card2>
+        <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={img2} alt="about us2" />
+      </s.Card2>
+    </>
   );
 };
 
